fae9b0d5cebe35dc2e72f770b0144696b1cd12e1
Prakriti Roy <prakritiroy201900@gmail.com>
2024-08-12 00:27:36 +0530

    Initial commit

---
+++ src/constant/index.js
@@ -0,0 +1,22 @@
+import project1 from "../assets/projects/project1.png"
+import project2 from "../assets/projects/project2.png"
+
+export const ABOUT_text = "Fresh B.Tech graduate of 2023 Batch. Skilled in Python and JavaScript as programming language, HTML/CSS as ascripting language, skilled in MySQL and MongoDB. Currently making myself proficient in React Js and DSA and seeking opportunities in software development and web development."
+
+export const PROJECTS = [
+    {
+        title: "Cryptyx",
+        image: project1,
+        description: "Build an online multiplayer web3 NFT card game, a decentralized application, that enables multiple users to play a card game using smart contracts."
+    },
+    {
+        title: "Clone of GPT-3",
+        image: project2,
+        description: "Build a fully responsive modern UI/UX GPT-3. This app is fully desktop and as well as mobile responsive"
+    }
+]
+
+export const contact = {
+    phoneNo: "7047770370",
+    email: "prakritiroy201900@gmail.com"
+}
